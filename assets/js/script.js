4db7b7c1936806734a3ef2a632701fd534b4cb4b
leozizz <leonardocps2015@gmail.com>
2022-05-18 11:17:57 -0300

    new JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,17 @@ let currentNumber = 0;
 
 function increment() {
     currentNumber += 1;
+    if (currentNumber >= 10){
+        currentNumber = 10
+    }
     currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement() {
     currentNumber -= 1;
+    if (currentNumber <= 0){
+        currentNumber = 0
+    }
     currentNumberWrapper.innerHTML = currentNumber;
 }
+
